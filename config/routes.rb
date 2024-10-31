cc69011ea490c2a4d061c43f36967a8862f2ac26
choward113 <choward@smith.edu>
2024-10-31 10:56:02 -0400

    Add user database migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "stations#index"
-  user :resources, only: [:create, :new, :update] # create = registrations, new = login
-  # Create is same as consoel create = save to db
+  user :resources, only: [:create, :new, :update, :index] # create = registrations, new = login
+  # maybe put login + registration as
+  # new = form ()
+  # Create is same as console create = save to db
+  # update ??
+  # index = read all users
   # new create user, but doesn't save
   #     --> use the temp user to check if a user in the db already exists with that user/password 
   #         --> authenticate
