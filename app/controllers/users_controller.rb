fd5d06516d260a62a50270bb9c26cdcd6b4de32d
SanjanaYasna <sanyasna517@gmail.com>
2024-11-01 19:32:37 -0400

    login doesn't work. I may have screwed this one up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,36 @@
-class UsersController < ApplicationController  
-  # Following is psuedocode, doesn't actually work
-  # def login(email, password):
-  #   @user = User.find_by(params[:email])
-  #   if @user.present? && password_correct?(password)
-  #     redirect_to(login)
-  #   end
+# class UsersController < ApplicationController  
+#   # Following is psuedocode, doesn't actually work
+#   # def login(email, password):
+#   #   @user = User.find_by(params[:email])
+#   #   if @user.present? && password_correct?(password)
+#   #     redirect_to(login)
+#   #   end
 
-  # def password_correct?(password):
-  #   if @user.pword == password:
-  #     return True
-  #   else
-  #     render(registration)
-  #     return False
+#   # def password_correct?(password):
+#   #   if @user.pword == password:
+#   #     return True
+#   #   else
+#   #     render(registration)
+#   #     return False
+# end
+
+class UsersController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  def create
+      user = User.create!(user_params)
+      session[:user_id] = user.id
+      render json: user, status: :created
+  end
+
+  def show
+      render json: @current_user
+  end
+
+  private
+
+  #not sure is this is right and passes in right number of params
+  def user_params
+      params.permit(:fname,:lname, :email, :phone, :pword)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   get 'users/signup'
   get 'users/login'
   root to: 'pages#index'
+
+  #post route for user signup , calling users_controller.rb create method
+  post 'users/signup', to: 'users#create' #create user session from user-controller.rb, else if login, below show whether user is authorized
+  #login should create a session
+  post 'users/login', to: 'sessions#create'
+  #TO DO: session destruction for logout
+  post 'users/logout', to: 'sessions#destroy'
 end
