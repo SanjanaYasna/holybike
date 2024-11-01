ac99cdf14fac15065b80bff8fba0c42c9f8067a2
SanjanaYasna <sanyasna517@gmail.com>
2024-11-01 19:32:37 -0400

    login doesn't work. I may have screwed this one up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,23 @@
 class User < ApplicationRecord
-    attr_accessor :fname,:lname, :email, :phone, :pword
-    def initialize(fname, lname, email, phone, pword)
-        @fname = fname
-        @lname = lname
-        @email = email 
-        @phone = phone
-        @pword = pword
-    end
+  #   attr_accessor :fname,:lname, :email, :phone, :pword
+  #   def initialize(fname, lname, email, phone, pword)
+  #       @fname = fname
+  #       @lname = lname
+  #       @email = email 
+  #       @phone = phone
+  #       @pword = pword
+  #   end
 
-  validates_presence_of    :identifier,
-                           :fname,
-                           :lname,
-                           :email,
-                           :phone,
-                           :pword
-  validates_uniqueness_of  :identifier, :email
-  
-  has_many :Bike, :Ride, :Broken_report, :Review, :Rental   #foreign_key: true (must correspond across tables)
+  # validates_presence_of    :fname,
+  #                          :lname,
+  #                          :email,
+  #                          :phone,
+  #                          :pword
+  validates_uniqueness_of :email, :phone
+  has_many :Bike
+  has_many :Ride 
+  has_many :Broken_report
+  has_many :Review
+  has_many :Rental   #foreign_key: true (must correspond across tables)
  
 end
