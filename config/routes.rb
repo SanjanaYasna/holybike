9a4554657965e2f31be3132d417738f0b404e447
choward113 <124058312+choward113@users.noreply.github.com>
2024-11-07 12:19:06 -0500

    Switched main to home and index to main

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'users/signup'
   get 'users/login'
-  root to: 'pages#main'
-  get 'pages/index'
+  root to: 'pages#home'
+  get 'pages/main'
+  
 
   resources :bikes,     only: [:index]
   resources :stations,  only: [:index]
