cca2076d05c9c922edfd1f7ecf267560c66941eb
SanjanaYasna <sanyasna517@gmail.com>
2024-11-09 19:09:07 -0500

    possible env changes to allow flash module, but still needs fixing. Commiting as opposed to stashing these changes

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Valetbike
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,30 @@
 Rails.application.routes.draw do
-  get 'users/signup'
+  get 'users/signup', to: 'users#create'
   get 'users/login'
-  root to: 'pages#main'
-  get 'pages/index'
+  root to: 'pages#placeholder'
+ # get 'pages/index'
 
   resources :bikes,     only: [:index]
   resources :stations,  only: [:index]
+  resources :registrations, only: [:new,:create]
+  resources :sessions, only: [:new, :create, :destroy]
+  
   
   #post route for user signup , calling users_controller.rb create method
-  post 'users/signup', to: 'users#create' #create user session from user-controller.rb, else if login, below show whether user is authorized
-  #login should create a session
-  post 'users/login', to: 'sessions#create'
-  #TO DO: session destruction for logout
-  post 'users/logout', to: 'sessions#destroy'
-end
+  # post 'users/signup', to: 'users#create' #create user session from user-controller.rb, else if login, below show whether user is authorized
+  # #login should create a session
+  # post 'users/login', to: 'sessions#create'
+  # #TO DO: session destruction for logout
+  # post 'users/logout', to: 'sessions#destroy'
 
+end
+#registrations/new <-create user for signup
+#registrations/create <-signup
+#sessions/create <-login
+#sessions/new <-create user for app
 #CAMILLE"S ROUTES, TO BE ADJUSTED
 # get 'users/signup', to: 'users#create'
 # get 'users/login'
 # #root to: 'pages#index'
-# resources :registrations, only: [:new,:create]
-# resources :sessions, only: [:new, :create, :destroy]
+
 # root to: 'pages#placeholder'
