df948e0a3e4342d3879acad2b29641b7513363dc
SanjanaYasna <sanyasna517@gmail.com>
2024-12-08 17:12:20 -0500

    mergingn sessions destruction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,17 +7,20 @@ class ApplicationController < ActionController::API
     before_action :authorize #comment and uncomment if unauthorized error appears.
     
     def current_user
-      @current_user ||= User.find_by(id: session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end 
     
     private
 
-    #check if the user is logged in by checking session id
+    #check if the user is logged in by checking session id (done with every controller except for exception actions)
     def authorize
       @current_user = User.find_by(id: session[:user_id])
       puts "User is nil: #{@current_user.nil?}"
       puts "Session User ID: #{session[:user_id]}"
-      render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+      unless @current_user
+          redirect_to "/home"
+      end
+      # render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
     end
   
     #render error message if user is not an admin
